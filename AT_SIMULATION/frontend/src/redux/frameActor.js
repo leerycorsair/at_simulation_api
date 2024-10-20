bc224e2a13409dc483b7485a23f04a981f8f31e9
grigadnal625 <grigandal625@gmail.com>
2024-10-21 02:01:23 +0300

    safe frame actor

--- AT_SIMULATION/frontend/src/redux/frameActor.js
+++ AT_SIMULATION/frontend/src/redux/frameActor.js
@@ -4,7 +4,18 @@ export const createFrameActionAsyncThunk = (type, payloadCreator, options) => {
     const newPayloadCreator = async (...args) => {
         const result = await payloadCreator(...args);
         if (window.sessionStorage.getItem("frameId")) {
-            window.parent.postMessage({ type: "action", event: type, data: { args: args[0], result }, frameId: window.sessionStorage.getItem("frameId") }, "*");
+            window.parent.postMessage(
+                {
+                    type: "action",
+                    event: type,
+                    data: {
+                        args: JSON.parse(JSON.stringify(args[0])),
+                        result: JSON.parse(JSON.stringify(result)),
+                    },
+                    frameId: window.sessionStorage.getItem("frameId"),
+                },
+                "*"
+            );
         }
         return result;
     };
