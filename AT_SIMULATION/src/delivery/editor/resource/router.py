af6b3de043c7018a3f74e916d12e69c50421bd5a
Leonov Vladislav <leonov.vladislav14@wb.ru>
2025-01-10 15:52:06 +0300

    fix:better router

--- AT_SIMULATION/src/delivery/editor/resource/router.py
+++ AT_SIMULATION/src/delivery/editor/resource/router.py
@@ -39,109 +39,161 @@ async def create_resource_type(
     body: ResourceTypeRequest,
     model_id: int = Depends(get_current_model),
     resource_service: IResourceService = Depends(get_resource_service),
-) -> ObjectIDResponse:
+) -> CommonResponse[ObjectIDResponse]:
     try:
         return SuccessResponse(
             to_ObjectIDResponse(
                 resource_service.create_resource_type(to_ResourceTypeDB(body, model_id))
             )
         )
-    except RuntimeError as e:
+    except Exception as e:
         return InternalServiceError(e)
 
 
-@router.get("/types", response_model=ResourceTypesResponse)
+@router.get("/types", response_model=CommonResponse[ResourceTypesResponse | None])
 async def get_resource_types(
     model_id: int = Depends(get_current_model),
     resource_service: IResourceService = Depends(get_resource_service),
-) -> ResourceTypesResponse:
-    return to_ResourceTypesResponse(resource_service.get_resource_types(model_id))
+) -> CommonResponse[ResourceTypesResponse]:
+    try:
+        return SuccessResponse(
+            to_ResourceTypesResponse(resource_service.get_resource_types(model_id))
+        )
+    except Exception as e:
+        return InternalServiceError(e)
 
 
-@router.get("/types/{resource_type_id}", response_model=ResourceTypeResponse)
+@router.get(
+    "/types/{resource_type_id}",
+    response_model=CommonResponse[ResourceTypeResponse | None],
+)
 async def get_resource_type(
     resource_type_id: int,
     model_id: int = Depends(get_current_model),
     resource_service: IResourceService = Depends(get_resource_service),
-) -> ResourceTypeResponse:
-    return to_ResourceTypeResponse(
-        resource_service.get_resource_type(resource_type_id, model_id)
-    )
+) -> CommonResponse[ResourceTypeResponse]:
+    try:
+        return SuccessResponse(
+            to_ResourceTypeResponse(
+                resource_service.get_resource_type(resource_type_id, model_id)
+            )
+        )
+    except Exception as e:
+        return InternalServiceError(e)
 
 
-@router.put("/types/{resource_type_id}", response_model=ObjectIDResponse)
+@router.put(
+    "/types/{resource_type_id}", response_model=CommonResponse[ObjectIDResponse | None]
+)
 async def update_resource_type(
     body: ResourceTypeRequest,
     model_id: int = Depends(get_current_model),
     resource_service: IResourceService = Depends(get_resource_service),
-) -> ObjectIDResponse:
-    return to_ObjectIDResponse(
-        resource_service.update_resource_type(
-            to_ResourceTypeDB(body, model_id),
+) -> CommonResponse[ObjectIDResponse]:
+    try:
+        return SuccessResponse(
+            to_ObjectIDResponse(
+                resource_service.update_resource_type(
+                    to_ResourceTypeDB(body, model_id),
+                )
+            )
         )
-    )
+    except Exception as e:
+        return InternalServiceError(e)
 
 
-@router.delete("/types/{resource_type_id}", response_model=ObjectIDResponse)
+@router.delete(
+    "/types/{resource_type_id}", response_model=CommonResponse[ObjectIDResponse | None]
+)
 async def delete_resource_type(
     resource_type_id: int,
     model_id: int = Depends(get_current_model),
     resource_service: IResourceService = Depends(get_resource_service),
-) -> ObjectIDResponse:
-    return to_ObjectIDResponse(
-        resource_service.delete_resource_type(resource_type_id, model_id)
-    )
+) -> CommonResponse[ObjectIDResponse]:
+    try:
+        return SuccessResponse(
+            to_ObjectIDResponse(
+                resource_service.delete_resource_type(resource_type_id, model_id)
+            )
+        )
+    except Exception as e:
+        return InternalServiceError(e)
 
 
-@router.post("/", response_model=ObjectIDResponse)
+@router.post("/", response_model=CommonResponse[ObjectIDResponse | None])
 async def create_resource(
     body: ResourceRequest,
     model_id: int = Depends(get_current_model),
     resource_service: IResourceService = Depends(get_resource_service),
-) -> ObjectIDResponse:
-    return to_ObjectIDResponse(
-        resource_service.create_resource(
-            to_ResourceDB(body, model_id),
+) -> CommonResponse[ObjectIDResponse]:
+    try:
+        return SuccessResponse(
+            to_ObjectIDResponse(
+                resource_service.create_resource(
+                    to_ResourceDB(body, model_id),
+                )
+            )
         )
-    )
+    except Exception as e:
+        return InternalServiceError(e)
 
 
-@router.get("/", response_model=ResourcesResponse)
+@router.get("/", response_model=CommonResponse[ResourcesResponse | None])
 async def get_resources(
     model_id: int = Depends(get_current_model),
     resource_service: IResourceService = Depends(get_resource_service),
-) -> ResourcesResponse:
-    return to_ResourcesResponse(
-        resource_service.get_resources(model_id),
-    )
+) -> CommonResponse[ResourcesResponse]:
+    try:
+        return SuccessResponse(
+            to_ResourcesResponse(
+                resource_service.get_resources(model_id),
+            )
+        )
+    except Exception as e:
+        return InternalServiceError(e)
 
 
-@router.get("/{resource_id}", response_model=ResourceResponse)
+@router.get("/{resource_id}", response_model=CommonResponse[ResourceResponse | None])
 async def get_resource(
     resource_id: int,
     model_id: int = Depends(get_current_model),
     resource_service: IResourceService = Depends(get_resource_service),
-) -> ResourceResponse:
-    return to_ResourceResponse(resource_service.get_resource(resource_id, model_id))
+) -> CommonResponse[ResourceResponse]:
+    try:
+        return SuccessResponse(
+            to_ResourceResponse(resource_service.get_resource(resource_id, model_id))
+        )
+    except Exception as e:
+        return InternalServiceError(e)
 
 
-@router.put("/{resource_id}", response_model=ObjectIDResponse)
+@router.put("/{resource_id}", response_model=CommonResponse[ObjectIDResponse | None])
 async def update_resource(
     body: ResourceRequest,
     model_id: int = Depends(get_current_model),
     resource_service: IResourceService = Depends(get_resource_service),
-) -> ObjectIDResponse:
-    return to_ObjectIDResponse(
-        resource_service.update_resource(
-            to_ResourceDB(body, model_id),
+) -> CommonResponse[ObjectIDResponse]:
+    try:
+        return SuccessResponse(
+            to_ObjectIDResponse(
+                resource_service.update_resource(
+                    to_ResourceDB(body, model_id),
+                )
+            )
         )
-    )
+    except Exception as e:
+        return InternalServiceError(e)
 
 
-@router.delete("/{resource_id}", response_model=ObjectIDResponse)
+@router.delete("/{resource_id}", response_model=CommonResponse[ObjectIDResponse | None])
 async def delete_resource(
     resource_id: int,
     model_id: int = Depends(get_current_model),
     resource_service: IResourceService = Depends(get_resource_service),
-) -> ObjectIDResponse:
-    return to_ObjectIDResponse(resource_service.delete_resource(resource_id, model_id))
+) -> CommonResponse[ObjectIDResponse]:
+    try:
+        return SuccessResponse(
+            to_ObjectIDResponse(resource_service.delete_resource(resource_id, model_id))
+        )
+    except Exception as e:
+        return InternalServiceError(e)
