e592d6648e0055873f434bbff017537974ba99db
Leonov Vladislav <leonov.vladislav14@wb.ru>
2025-01-14 19:52:44 +0300

    feat:visio service

--- AT_SIMULATION/src/service/visio/dependencies.py
+++ AT_SIMULATION/src/service/visio/dependencies.py
@@ -2,7 +2,13 @@ from typing import List, Protocol
 
 from fastapi import Depends
 
-from src.repository.visio.models.models import EdgeDB, NodeDB, NodeTablesEnum
+from src.repository.visio.models.models import (
+    EdgeDB,
+    EditorInfoDB,
+    MoveNodeDB,
+    NodeDB,
+    NodeTablesEnum,
+)
 from src.repository.visio.repository import VisioRepository
 from sqlalchemy.orm import Session
 
@@ -16,6 +22,8 @@ class IVisioRepository(Protocol):
 
     def get_node(self, object_table: NodeTablesEnum, object_id: int) -> NodeDB: ...
 
+    def get_node_by_id(self, node_id: int) -> NodeDB: ...
+
     def delete_node(self, object_table: NodeTablesEnum, object_id: int) -> int: ...
 
     def get_nodes(self, model_id: int) -> List[NodeDB]: ...
@@ -23,6 +31,10 @@ class IVisioRepository(Protocol):
     def create_edge(self, edge: EdgeDB) -> int: ...
 
     def get_edges(self, model_id: int) -> List[EdgeDB]: ...
+
+    def get_editor_info(self, model_id: int) -> EditorInfoDB: ...
+
+    def move_node(self, params: MoveNodeDB) -> None: ...
 
 
 def get_visio_repository(session: Session = Depends(get_db)) -> IVisioRepository:
--- AT_SIMULATION/src/service/visio/service.py
+++ AT_SIMULATION/src/service/visio/service.py
@@ -1,5 +1,10 @@
 from src.repository.visio.models.conversions import to_NodeTablesEnum_from_node_type
-from src.repository.visio.models.models import NodeDB, NodeTypesEnum
+from src.repository.visio.models.models import (
+    EditorInfoDB,
+    MoveNodeDB,
+    NodeDB,
+    NodeTypesEnum,
+)
 from src.service.visio.conversions import to_EdgeDB, to_NodeDB
 from src.service.visio.dependencies import IVisioRepository
 
@@ -11,6 +16,11 @@ class VisioService:
         visio_rep: IVisioRepository,
     ) -> None:
         self._visio_rep = visio_rep
+
+    def _check_node_rights(self, node_id: int, model_id: int) -> None:
+        node = self._visio_rep.get_node_by_id(node_id)
+        if node.model_id != model_id:
+            raise ValueError(f"Node {node_id} does not belong to model {model_id}")
 
     def create_node(
         self, object_id: int, object_name: str, node_type: NodeTypesEnum, model_id: int
@@ -37,3 +47,10 @@ class VisioService:
     def create_edge(self, from_id: int, to_id: int, model_id: int) -> int:
         edge = to_EdgeDB(from_id, to_id, model_id)
         return self._visio_rep.create_edge(edge)
+
+    def get_editor_info(self, model_id: int) -> EditorInfoDB:
+        return self.get_editor_info(model_id)
+
+    def move_node(self, params: MoveNodeDB, model_id: int) -> None:
+        self._check_node_rights(params.node_id, model_id)
+        return self.move_node(params, model_id)
