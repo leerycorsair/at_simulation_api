9a2defa9167e98dd90790a62526e8b2d7a5ceef7
Leonov Vladislav <leonov.vladislav14@wb.ru>
2025-01-16 03:40:26 +0300

    feat:convs

---
+++ AT_SIMULATION/src/delivery/translator/models/conversions.py
@@ -0,0 +1,30 @@
+from typing import List
+from src.delivery.translator.models.models import (
+    TranslateResponse,
+    TranslatedFileResponse,
+    TranslatedFilesResponse,
+)
+from src.service.translator.models.models import FileMeta, TranslateInfo
+
+
+def to_TranslatedFileResponse(meta: FileMeta) -> TranslatedFileResponse:
+    return TranslatedFileResponse(
+        id=meta.id,
+        name=meta.name,
+        model_id=meta.model_id,
+    )
+
+
+def to_TranslatedFilesResponse(metas: List[FileMeta]) -> TranslatedFilesResponse:
+    return TranslatedFilesResponse(
+        files=[to_TranslatedFileResponse(meta) for meta in metas],
+        total=len(metas),
+    )
+
+
+def to_TranslateResponse(info: TranslateInfo) -> TranslateResponse:
+    return TranslateResponse(
+        file_id=info.file_id,
+        file_content=info.file_content,
+        translate_logs=info.translate_logs,
+    )
