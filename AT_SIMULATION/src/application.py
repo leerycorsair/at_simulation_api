023b09d92ad44fb2a0f390e938bb216d362f4c0b
Leonov Vladislav <leonov.vladislav14@wb.ru>
2025-01-23 16:36:51 +0300

    fix

--- AT_SIMULATION/src/application.py
+++ AT_SIMULATION/src/application.py
@@ -32,11 +32,9 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="AT_SIMULATION", version="1.0.0", lifespan=lifespan)
 
-origins = ["*"]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -46,17 +44,13 @@ app.add_middleware(
 # Configure logging
 logger = logging.getLogger(__name__)
 
+
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    logger.error(
-        f"Validation error at {request.url}: {exc.errors()}",
-        exc_info=True  
-    )
+    logger.error(f"Validation error at {request.url}: {exc.errors()}", exc_info=True)
 
     # Prepare the response
-    response_content = BadRequestError(
-        exception=Exception("Invalid request body.")
-    )
+    response_content = BadRequestError(exception=Exception("Invalid request body."))
     return JSONResponse(
         status_code=response_content.status_code,
         content=response_content.model_dump(),
