70fbeb7f1eaafa6b39a50eb3c126a4d45c8e75a6
Leonov Vladislav <leonov.vladislav14@wb.ru>
2025-01-25 19:46:22 +0300

    feat:better imports router

--- AT_SIMULATION/src/delivery/editor/imports/router.py
+++ AT_SIMULATION/src/delivery/editor/imports/router.py
@@ -1,13 +1,19 @@
 from fastapi import APIRouter, Depends
 from src.delivery.core.models.conversions import (
-    InternalServiceError,
-    SuccessResponse,
     to_ObjectIDResponse,
 )
-from src.delivery.core.models.models import CommonResponse, ObjectIDResponse
+from src.delivery.core.models.models import ObjectIDResponse
 from src.delivery.editor.imports.dependencies import IImportService, get_import_service
-from src.delivery.editor.imports.models.conversions import to_ImportDB, to_ImportResponse, to_ImportsResponse
-from src.delivery.editor.imports.models.models import ImportRequest, ImportResponse, ImportsResponse
+from src.delivery.editor.imports.models.conversions import (
+    to_ImportDB,
+    to_ImportResponse,
+    to_ImportsResponse,
+)
+from src.delivery.editor.imports.models.models import (
+    ImportRequest,
+    ImportResponse,
+    ImportsResponse,
+)
 from src.delivery.model.dependencies import get_current_model
 
 router = APIRouter(
@@ -16,72 +22,49 @@ router = APIRouter(
 )
 
 
-@router.post("", response_model=CommonResponse[ObjectIDResponse | None])
+@router.post("", response_model=ObjectIDResponse)
 async def create_import(
     body: ImportRequest,
     model_id: int = Depends(get_current_model),
     import_service: IImportService = Depends(get_import_service),
-) -> CommonResponse[ObjectIDResponse]:
-    try:
-        return SuccessResponse(
-            to_ObjectIDResponse(
-                import_service.create_import(to_ImportDB(body, model_id))
-            )
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> ObjectIDResponse:
+    return to_ObjectIDResponse(
+        import_service.create_import(to_ImportDB(body, model_id))
+    )
 
 
-@router.get("", response_model=CommonResponse[ImportsResponse | None])
+@router.get("", response_model=ImportsResponse)
 async def get_imports(
     model_id: int = Depends(get_current_model),
     import_service: IImportService = Depends(get_import_service),
-) -> CommonResponse[ImportsResponse]:
-    try:
-        return SuccessResponse(to_ImportsResponse(import_service.get_imports(model_id)))
-    except Exception as e:
-        return InternalServiceError(e)
+) -> ImportsResponse:
+    return to_ImportsResponse(import_service.get_imports(model_id))
 
 
-@router.get("/{import_id}", response_model=CommonResponse[ImportResponse | None])
+@router.get("/{import_id}", response_model=ImportResponse)
 async def get_import(
     import_id: int,
     model_id: int = Depends(get_current_model),
     import_service: IImportService = Depends(get_import_service),
-) -> CommonResponse[ImportResponse]:
-    try:
-        return SuccessResponse(
-            to_ImportResponse(import_service.get_import(import_id, model_id))
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> ImportResponse:
+    return to_ImportResponse(import_service.get_import(import_id, model_id))
 
 
-@router.put("/{import_id}", response_model=CommonResponse[ObjectIDResponse | None])
+@router.put("/{import_id}", response_model=ObjectIDResponse)
 async def update_import(
     body: ImportRequest,
     model_id: int = Depends(get_current_model),
     import_service: IImportService = Depends(get_import_service),
-) -> CommonResponse[ObjectIDResponse]:
-    try:
-        return SuccessResponse(
-            to_ObjectIDResponse(
-                import_service.update_import(to_ImportDB(body, model_id))
-            )
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> ObjectIDResponse:
+    return to_ObjectIDResponse(
+        import_service.update_import(to_ImportDB(body, model_id))
+    )
 
 
-@router.delete("/{import_id}", response_model=CommonResponse[ObjectIDResponse | None])
+@router.delete("/{import_id}", response_model=ObjectIDResponse)
 async def delete_import(
     import_id: int,
     model_id: int = Depends(get_current_model),
     import_service: IImportService = Depends(get_import_service),
-) -> CommonResponse[ObjectIDResponse]:
-    try:
-        return SuccessResponse(
-            to_ObjectIDResponse(import_service.delete_import(import_id, model_id))
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> ObjectIDResponse:
+    return to_ObjectIDResponse(import_service.delete_import(import_id, model_id))
