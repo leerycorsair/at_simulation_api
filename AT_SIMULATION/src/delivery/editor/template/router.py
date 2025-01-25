5e1788c860c9f04bf9387dfff5b1937e4a7f9df9
Leonov Vladislav <leonov.vladislav14@wb.ru>
2025-01-25 20:00:03 +0300

    feat:better template router

--- AT_SIMULATION/src/delivery/editor/template/router.py
+++ AT_SIMULATION/src/delivery/editor/template/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, Depends
 
-from src.delivery.core.models.conversions import InternalServiceError, SuccessResponse, to_ObjectIDResponse
-from src.delivery.core.models.models import CommonResponse, ObjectIDResponse
+from src.delivery.core.models.conversions import (
+    to_ObjectIDResponse,
+)
+from src.delivery.core.models.models import ObjectIDResponse
 from src.delivery.editor.template.dependencies import (
     ITemplateService,
     get_template_service,
@@ -40,261 +42,161 @@ router = APIRouter(
 )
 
 
-@router.post("/irregular_event", response_model=CommonResponse[ObjectIDResponse | None])
+@router.post("/irregular_event", response_model=ObjectIDResponse)
 async def create_irregular_event(
     body: IrregularEventRequest,
     model_id: int = Depends(get_current_model),
     template_service: ITemplateService = Depends(get_template_service),
-) -> CommonResponse[ObjectIDResponse]:
-    try:
-        return SuccessResponse(
-            to_ObjectIDResponse(
-                template_service.create_irregular_event(
-                    to_IrregularEventDB(body, model_id)
-                )
-            )
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> ObjectIDResponse:
+    return to_ObjectIDResponse(
+        template_service.create_irregular_event(to_IrregularEventDB(body, model_id))
+    )
 
 
-@router.post("/operation", response_model=CommonResponse[ObjectIDResponse | None])
+@router.post("/operation", response_model=ObjectIDResponse)
 async def create_operation(
     body: OperationRequest,
     model_id: int = Depends(get_current_model),
     template_service: ITemplateService = Depends(get_template_service),
-) -> CommonResponse[ObjectIDResponse]:
-    try:
-        return SuccessResponse(
-            to_ObjectIDResponse(
-                template_service.create_operation(to_OperationDB(body, model_id))
-            )
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> ObjectIDResponse:
+    return to_ObjectIDResponse(
+        template_service.create_operation(to_OperationDB(body, model_id))
+    )
 
 
-@router.post("/rule", response_model=CommonResponse[ObjectIDResponse | None])
+@router.post("/rule", response_model=ObjectIDResponse)
 async def create_rule(
     body: RuleRequest,
     model_id: int = Depends(get_current_model),
     template_service: ITemplateService = Depends(get_template_service),
-) -> CommonResponse[ObjectIDResponse]:
-    try:
-        return SuccessResponse(
-            to_ObjectIDResponse(template_service.create_rule(to_RuleDB(body, model_id)))
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> ObjectIDResponse:
+    return to_ObjectIDResponse(template_service.create_rule(to_RuleDB(body, model_id)))
 
 
-@router.get("", response_model=CommonResponse[TemplatesResponse | None])
+@router.get("", response_model=TemplatesResponse)
 async def get_templates(
     model_id: int = Depends(get_current_model),
     template_service: ITemplateService = Depends(get_template_service),
-) -> CommonResponse[TemplatesResponse]:
-    try:
-        return SuccessResponse(
-            to_TemplatesResponse(template_service.get_templates(model_id))
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> TemplatesResponse:
+    return to_TemplatesResponse(template_service.get_templates(model_id))
 
 
-@router.get(
-    "/{template_id}/irregular_event",
-    response_model=CommonResponse[IrregularEventResponse | None],
-)
+@router.get("/{template_id}/irregular_event", response_model=IrregularEventResponse)
 async def get_irregular_event(
     template_id: int,
     model_id: int = Depends(get_current_model),
     template_service: ITemplateService = Depends(get_template_service),
-) -> CommonResponse[IrregularEventResponse]:
-    try:
-        return SuccessResponse(
-            to_IrregularEventResponse(
-                template_service.get_irregular_event(template_id, model_id)
-            )
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> IrregularEventResponse:
+    return to_IrregularEventResponse(
+        template_service.get_irregular_event(template_id, model_id)
+    )
 
 
-@router.get(
-    "/{template_id}/operation", response_model=CommonResponse[OperationResponse | None]
-)
+@router.get("/{template_id}/operation", response_model=OperationResponse)
 async def get_operation(
     template_id: int,
     model_id: int = Depends(get_current_model),
     template_service: ITemplateService = Depends(get_template_service),
-) -> CommonResponse[OperationResponse]:
-    try:
-        return SuccessResponse(
-            to_OperationResponse(template_service.get_operation(template_id, model_id))
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> OperationResponse:
+    return to_OperationResponse(template_service.get_operation(template_id, model_id))
 
 
-@router.get("/{template_id}/rule", response_model=CommonResponse[RuleResponse | None])
+@router.get("/{template_id}/rule", response_model=RuleResponse)
 async def get_rule(
     template_id: int,
     model_id: int = Depends(get_current_model),
     template_service: ITemplateService = Depends(get_template_service),
-) -> CommonResponse[RuleResponse]:
-    try:
-        return SuccessResponse(
-            to_RuleResponse(template_service.get_rule(template_id, model_id))
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> RuleResponse:
+    return to_RuleResponse(template_service.get_rule(template_id, model_id))
 
 
-@router.put(
-    "/{template_id}/irregular_event",
-    response_model=CommonResponse[ObjectIDResponse | None],
-)
+@router.put("/{template_id}/irregular_event", response_model=ObjectIDResponse)
 async def update_irregular_event(
     body: IrregularEventRequest,
     model_id: int = Depends(get_current_model),
     template_service: ITemplateService = Depends(get_template_service),
-) -> CommonResponse[ObjectIDResponse]:
-    try:
-        return SuccessResponse(
-            to_ObjectIDResponse(
-                template_service.update_irregular_event(
-                    to_IrregularEventDB(body, model_id)
-                )
-            )
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> ObjectIDResponse:
+    return to_ObjectIDResponse(
+        template_service.update_irregular_event(to_IrregularEventDB(body, model_id))
+    )
 
 
-@router.put(
-    "/{template_id}/operation", response_model=CommonResponse[ObjectIDResponse | None]
-)
+@router.put("/{template_id}/operation", response_model=ObjectIDResponse)
 async def update_operation(
     body: OperationRequest,
     model_id: int = Depends(get_current_model),
     template_service: ITemplateService = Depends(get_template_service),
-) -> CommonResponse[ObjectIDResponse]:
-    try:
-        return SuccessResponse(
-            to_ObjectIDResponse(
-                template_service.update_operation(to_OperationDB(body, model_id))
-            )
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> ObjectIDResponse:
+    return to_ObjectIDResponse(
+        template_service.update_operation(to_OperationDB(body, model_id))
+    )
 
 
-@router.put(
-    "/{template_id}/rule", response_model=CommonResponse[ObjectIDResponse | None]
-)
+@router.put("/{template_id}/rule", response_model=ObjectIDResponse)
 async def update_rule(
     body: RuleRequest,
     model_id: int = Depends(get_current_model),
     template_service: ITemplateService = Depends(get_template_service),
-) -> CommonResponse[ObjectIDResponse]:
-    try:
-        return SuccessResponse(
-            to_ObjectIDResponse(template_service.update_rule(to_RuleDB(body, model_id)))
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> ObjectIDResponse:
+    return to_ObjectIDResponse(template_service.update_rule(to_RuleDB(body, model_id)))
 
 
-@router.delete("/{template_id}", response_model=CommonResponse[ObjectIDResponse | None])
+@router.delete("/{template_id}", response_model=ObjectIDResponse)
 async def delete_template(
     template_id: int,
     model_id: int = Depends(get_current_model),
     template_service: ITemplateService = Depends(get_template_service),
-) -> CommonResponse[ObjectIDResponse]:
-    try:
-        return SuccessResponse(
-            to_ObjectIDResponse(template_service.delete_template(template_id, model_id))
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> ObjectIDResponse:
+    return to_ObjectIDResponse(template_service.delete_template(template_id, model_id))
 
 
-@router.post("/usages", response_model=CommonResponse[ObjectIDResponse | None])
+@router.post("/usages", response_model=ObjectIDResponse)
 async def create_usage(
     body: TemplateUsageRequest,
     model_id: int = Depends(get_current_model),
     template_service: ITemplateService = Depends(get_template_service),
-) -> CommonResponse[ObjectIDResponse]:
-    try:
-        return SuccessResponse(
-            to_ObjectIDResponse(
-                template_service.create_template_usage(
-                    to_TemplateUsageDB(body, model_id)
-                )
-            )
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> ObjectIDResponse:
+    return to_ObjectIDResponse(
+        template_service.create_template_usage(to_TemplateUsageDB(body, model_id))
+    )
 
 
-@router.get("/usages", response_model=CommonResponse[TemplateUsagesResponse | None])
+@router.get("/usages", response_model=TemplateUsagesResponse)
 async def get_usages(
     model_id: int = Depends(get_current_model),
     template_service: ITemplateService = Depends(get_template_service),
-) -> CommonResponse[TemplateUsagesResponse]:
-    try:
-        return SuccessResponse(
-            to_TemplateUsagesResponse(template_service.get_template_usages(model_id))
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> TemplateUsagesResponse:
+    return to_TemplateUsagesResponse(template_service.get_template_usages(model_id))
 
 
-@router.get("/usages/{usage_id}", response_model=CommonResponse[TemplateUsageResponse | None])
+@router.get("/usages/{usage_id}", response_model=TemplateUsageResponse)
 async def get_usage(
     usage_id: int,
     model_id: int = Depends(get_current_model),
     template_service: ITemplateService = Depends(get_template_service),
-) -> CommonResponse[TemplateUsageResponse]:
-    try:
-        return SuccessResponse(
-            to_TemplateUsageResponse(
-                template_service.get_template_usage(usage_id, model_id)
-            )
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> TemplateUsageResponse:
+    return to_TemplateUsageResponse(
+        template_service.get_template_usage(usage_id, model_id)
+    )
 
 
-@router.put("/usages/{usage_id}", response_model=CommonResponse[ObjectIDResponse | None])
+@router.put("/usages/{usage_id}", response_model=ObjectIDResponse)
 async def update_usage(
     body: TemplateUsageRequest,
     model_id: int = Depends(get_current_model),
     template_service: ITemplateService = Depends(get_template_service),
-) -> CommonResponse[ObjectIDResponse]:
-    try:
-        return SuccessResponse(
-            to_ObjectIDResponse(
-                template_service.update_template_usage(
-                    to_TemplateUsageDB(body, model_id)
-                )
-            )
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> ObjectIDResponse:
+    return to_ObjectIDResponse(
+        template_service.update_template_usage(to_TemplateUsageDB(body, model_id))
+    )
 
 
-@router.delete("/usages/{usage_id}", response_model=CommonResponse[ObjectIDResponse | None])
+@router.delete("/usages/{usage_id}", response_model=ObjectIDResponse)
 async def delete_usage(
     usage_id: int,
     model_id: int = Depends(get_current_model),
     template_service: ITemplateService = Depends(get_template_service),
-) -> CommonResponse[ObjectIDResponse]:
-    try:
-        return SuccessResponse(
-            to_ObjectIDResponse(
-                template_service.delete_template_usage(usage_id, model_id)
-            )
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> ObjectIDResponse:
+    return to_ObjectIDResponse(
+        template_service.delete_template_usage(usage_id, model_id)
+    )
