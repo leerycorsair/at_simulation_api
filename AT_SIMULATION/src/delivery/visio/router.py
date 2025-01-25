dbb1f6078c30aee6d9e3f7661e0d6a64129cde67
Leonov Vladislav <leonov.vladislav14@wb.ru>
2025-01-25 20:06:58 +0300

    fix

--- AT_SIMULATION/src/delivery/visio/router.py
+++ AT_SIMULATION/src/delivery/visio/router.py
@@ -1,6 +1,4 @@
 from fastapi import APIRouter, Depends
-from src.delivery.core.models.conversions import InternalServiceError, SuccessResponse
-from src.delivery.core.models.models import CommonResponse
 from src.delivery.model.dependencies import get_current_model
 from src.delivery.visio.dependencies import IVisioService, get_visio_service
 from src.delivery.visio.models.conversions import to_EditorInfoResponse, to_MoveNodeDB
@@ -12,28 +10,19 @@ router = APIRouter(
 )
 
 
-@router.get("/editor/info", response_model=CommonResponse[EditorInfoResponse | None])
+@router.get("/editor/info", response_model=EditorInfoResponse)
 async def get_editor_info(
     model_id: int = Depends(get_current_model),
     visio_service: IVisioService = Depends(get_visio_service),
-) -> CommonResponse[EditorInfoResponse]:
-    try:
-        return SuccessResponse(
-            to_EditorInfoResponse(visio_service.get_editor_info(model_id))
-        )
-    except Exception as e:
-        return InternalServiceError(e)
+) -> EditorInfoResponse:
+    return to_EditorInfoResponse(visio_service.get_editor_info(model_id))
 
 
-@router.patch("/editor/nodes/{node_id}/move", response_model=CommonResponse[None])
+@router.patch("/editor/nodes/{node_id}/move", response_model=None)
 async def move_node(
     body: MoveNodeRequest,
     node_id: int,
     model_id: int = Depends(get_current_model),
     visio_service: IVisioService = Depends(get_visio_service),
-) -> CommonResponse[None]:
-    try:
-        visio_service.move_node(to_MoveNodeDB(body, node_id), model_id)
-        return SuccessResponse(None)
-    except Exception as e:
-        return InternalServiceError(e)
+) -> None:
+    visio_service.move_node(to_MoveNodeDB(body, node_id), model_id)
