2f2a1552be57b2f18ed82c73b056472f36ce3de5
leerycorsair <vladleonov21032002@gmail.com>
2025-03-19 19:15:12 +0300

    fix:component_method -> authorized_method

--- src/worker/worker.py
+++ src/worker/worker.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from at_queue.core.at_component import ATComponent
-from at_queue.utils.decorators import component_method
+from at_queue.utils.decorators import authorized_method
 
 from src.worker.dependencies import (
     IAuthClient,
@@ -34,33 +34,33 @@ class ATSimulationWorker(ATComponent):
         self._translator_service = translator_service
         self._processor_service = processor_service
 
-    @component_method
-    async def get_translated_files(self, user_token: str) -> List[TranslatedFileDict]:
-        user_id = await self._auth_client.verify_token(user_token)
+    @authorized_method
+    async def get_translated_files(self, auth_token: str) -> List[TranslatedFileDict]:
+        user_id = await self._auth_client.verify_token(auth_token)
         models = self._model_service.get_models(user_id)
         files = self._translator_service.get_translated_files(user_id)
         return to_TranslatedFileDicts(files, models)
 
-    @component_method
+    @authorized_method
     async def create_process(
-        self, user_token: str, file_id: str, process_name: str
+        self, auth_token: str, file_id: str, process_name: str
     ) -> ProcessDict:
-        user_id = await self._auth_client.verify_token(user_token)
+        user_id = await self._auth_client.verify_token(auth_token)
         return to_ProcessDict(
             self._processor_service.create_process(user_id, file_id, process_name)
         )
 
-    @component_method
-    async def run_tick(self, user_token: str, process_id: str) -> TickDict:
-        user_id = await self._auth_client.verify_token(user_token)
+    @authorized_method
+    async def run_tick(self, auth_token: str, process_id: str) -> TickDict:
+        user_id = await self._auth_client.verify_token(auth_token)
         return to_TickDict(await self._processor_service.run_tick(user_id, process_id))
 
-    @component_method
-    async def kill_process(self, user_token: str, process_id: str) -> ProcessDict:
-        user_id = await self._auth_client.verify_token(user_token)
+    @authorized_method
+    async def kill_process(self, auth_token: str, process_id: str) -> ProcessDict:
+        user_id = await self._auth_client.verify_token(auth_token)
         return to_ProcessDict(self._processor_service.kill_process(user_id, process_id))
 
-    @component_method
-    async def get_processes(self, user_token: str) -> List[ProcessDict]:
-        user_id = await self._auth_client.verify_token(user_token)
+    @authorized_method
+    async def get_processes(self, auth_token: str) -> List[ProcessDict]:
+        user_id = await self._auth_client.verify_token(auth_token)
         return to_ProcessDicts(self._processor_service.get_processes(user_id))
